8f2028c757bf3300d2502e9a317dd71c5b313b9d
FacundoStorani <storanif@udesa.edu.ar>
2023-11-10 12:22:19 -0300

    Modificando js y conexion con htmls

--- Js/home.js
+++ Js/home.js
@@ -4,7 +4,8 @@ let movieData = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}
 
 /* Caputando elementos del DOM */
 
-let listaPeliculas = document.querySelector(".divhome");
+let listaPeliculas = document.querySelector(".divhomePeliculas");
+
 console.log(listaPeliculas);
 
 fetch(movieData)
@@ -13,13 +14,79 @@ fetch(movieData)
     return response.json();
 })
 .then(function(data) {
-    for (let index = 0; index < array.length; index++) {
-        console
-
-        
+    console.log(data.results);
+    let contenido = "";
+    for (let index = 0; index < 5 ; index++) {
+        contenido += `
+                                    <a class="aHome" href="./detalleSerie.html" > 
+                                        <article class="ArtHome"> 
+                                        <img class="imgHome" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}"Breaking Bad">   
+                                        <p>${data.results[index].title}</p>
+                                        </article>
+                                    </a>`   
     }
 
-    console.log(data.results); /*una array*/
+    listaPeliculas.innerHTML = contenido;
+
+    return data; 
+})
+.catch(function(errors) {
+    console.log(errors);
+    return errors;
+})
+
+/* Peliculas Populares */
+
+let peliculaPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+let listaPopulares = document.querySelector(".divhome");
+console.log(listaPopulares);
+
+fetch(peliculaPopular)
+
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data.results);
+    for (let index = 0; index < 5 ; index++) {
+        listaPopulares.innerHTML += `
+                                      <a class="aHome" href="./detallePelicula.html" > 
+                                         <article class="ArtHome"> 
+                                           <img class="imgHome" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="Spiderman">  
+                                           <p>${data.results[index].title}</p> 
+                                         </article>
+                                      </a>`   
+    }
+
+    return data; 
+})
+.catch(function(errors) {
+    console.log(errors);
+    return errors;
+})
+
+/* series tv*/
+
+let serieTV = `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}`;
+let listaSerieTV = document.querySelector(".divhome");
+console.log(listaSerieTV);
+
+fetch(serieTV)
+
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data.results);
+    for (let index = 0; index < 5 ; index++) {
+        listaSerieTV.innerHTML += `
+                                   <a class="aHome" href="./detallePelicula.html" >
+                                       <article class="ArtHome"> 
+                                           <img class="imgHome" src="./Img/Casino.jpg" alt="Casino">
+                                       </article>
+                                   </a>`   
+    }
+
     return data; 
 })
 .catch(function(errors) {
